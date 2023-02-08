db4cede33bb94267c0bbee00500d28ad0d32feed
ads2002 <91458369+ads2002@users.noreply.github.com>
2023-02-08 21:42:01 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Javascript is an _______ language?",
+    answer: "A. Object Oriented",
+    options: [
+      "A. Object Oriented",
+      "B. Object Based",
+      "C. Procedural",
+      "D. None Of The Above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answer: "C. Both A and B",
+    options: [
+      "A. var",
+      "B. let",
+      "C. Both A and B",
+      "D. None Of The Above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How can a datatype be declared to be a constant type?",
+    answer: "A. const",
+    options: [
+      "A. const",
+      "B. var",
+      "C. let",
+      "D. constant"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which one of the following also known as Conditional Expression?",
+    answer: "D. Immediate if",
+    options: [
+      "A. Alternative to if-else",
+      "B. Switch statement",
+      "C. If-then-else statement",
+      "D. Immediate if"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following are closures in Javascript?",
+    answer: "D. All Of The Above",
+    options: [
+      "A. Variables",
+      "B. Functions",
+      "C. Objects",
+      "D. All Of The Above"
+    ]
+  },
+];
